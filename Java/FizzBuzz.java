8a36501bd7395d1b8e840cd807220a291557e07e
graycoats <kurtsplat@gmail.com>
2015-06-03 15:07:31 -0400

    Update FizzBuzz.java
    took out zero option

--- Java/FizzBuzz.java
+++ Java/FizzBuzz.java
@@ -1,12 +1,21 @@
 /*
 Write a program that prints the numbers from 1 to 100.
-But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
-For numbers which are multiples of both three and five print “FizzBuzz”.
+But for multiples of three print â€œFizzâ€ instead of the number and for the multiples of five print â€œBuzzâ€.
+For numbers which are multiples of both three and five print â€œFizzBuzzâ€.
 */
 
-public class FizzBuzz{
-	public static void main(String[] args){
-		String fb = "FizzBuzz";
-		for(int i = 0; i < 100; i++, System.out.println(i % 3 == 0 && i % 5 == 0 ? fb : i % 3 == 0 ? fb.substring(0,4) : i % 5 == 0 ? fb.substring(4,fb.length()) : i));
+public class FizzBuzz
+{
+	public static void main(String[] args)
+	{
+        	for (int i = 1; i <= 100; i++)
+			if (i % 3 == 0 && i % 5 == 0) //or i % 15 == 0
+                		System.out.println("fizzbuzz");
+            		else if (i % 3 == 0)
+                		System.out.println("fizz");
+            		else if (i % 5 == 0)
+                		System.out.println("buzz");
+            		else
+                		System.out.println(i);
 	}
 }
